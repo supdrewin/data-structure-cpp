7308d4c1455c24c5f1559c48c6d127b9eef06dfd
supdrewin <supdrewin@outlook.com>
2021-11-04 17:02:30 +0800

    The `Templates` at $HOME

---
+++ Templates/template.c
@@ -0,0 +1,20 @@
+/* ------------------- template.c -------------------
+ * This is a template for C written by Supdrewin.     *
+ * Follow me on Github: https://github.com/supdrewin. *
+   ------------------- template.c ------------------- */
+
+// Standard I/O header
+#include <stdio.h>
+
+// Standard library header
+#include <stdlib.h>
+
+// Start main function
+int main(int argc, char **argv) {
+  // TODO: ...
+
+  // Exit with status 0
+  return 0;
+}
+
+// vim:set nu rnu cuc cul ts=2 sw=2 et si:
---
+++ Templates/template.h
@@ -0,0 +1,13 @@
+/* --------------------- template.h ---------------------
+ * This is a template for C headers written by Supdrewin. *
+ * Follow me on Github: https://github.com/supdrewin.     *
+   --------------------- template.h --------------------- */
+
+#ifndef _template_h_
+#define _template_h_
+
+// TODO: ...
+
+#endif /* _template_h_ */
+
+// vim:set nu rnu cuc cul ts=2 sw=2 et si:
