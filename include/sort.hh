d486f1e68efa45b6a63d923246b529c422c2e982
Supdrewin <supdrewin@outlook.com>
2021-12-13 17:11:45 +0800

    backup

--- include/sort.hh
+++ include/sort.hh
@@ -1,16 +1,15 @@
-/**
- * Copyright(C) 2021 Supdrewin <https://github.com/supdrewin>
- * sort.hh is part of Sort Algorithm Example.
- * The Sort Algorithm Example is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your
+/** sort.hh -*- C++ -*- Copyright (C) 2021 Supdrewin
+ * This file is part of the Data Structure Cpp Project.
+ * This Cpp project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- * The Sort Algorithm Example is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABI-
- * LITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * This Cpp project is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef __sort_hh
