768ca99e6b08038f103571620432d575b85c193a
Supdrewin <supdrewin@outlook.com>
2021-12-14 00:04:15 +0800

    backup

--- include/sort.hpp
+++ include/sort.hpp
@@ -136,7 +136,7 @@ template <typename type, unsigned size> struct array {
 };
 
 template <typename type, unsigned size>
-void sort(array<type, size> &arr, bool reverse = false) {
+inline void sort(array<type, size> &arr, bool reverse = false) {
   using std::sort;
   sort(arr.begin(), arr.end(),
        [reverse](base_data<type> &a, base_data<type> &b) {
