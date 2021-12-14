916923eac40b00bd1d25e1061bc5d80b4a8e0752
Supdrewin <supdrewin@outlook.com>
2021-12-14 14:36:00 +0800

    backup

--- include/deque.hpp
+++ include/deque.hpp
@@ -23,8 +23,6 @@ template <class _Tp> class deque {
 public:
   using size_type = unsigned;
   using value_type = _Tp;
-  using reference = value_type &;
-  using const_reference = const value_type &;
 
 protected:
   size_type __count, __offset, __max_size;
@@ -45,7 +43,7 @@ public:
 
     auto tmp = new value_type[__n];
     for (unsigned i{}; i < __count; ++i)
-      tmp[i] = __data[i + __offset];
+      tmp[i] = __data[(i + __offset) % __max_size];
 
     this->__data.reset(tmp);
     this->__max_size = __n;
@@ -56,42 +54,34 @@ public:
 
   bool empty() const noexcept { return this->size() == 0; }
 
-  reference operator[](size_type __i) noexcept { return this->__data[__i]; }
-
-  const_reference operator[](size_type __i) const noexcept {
-    return this->__data[__i];
-  }
-
-  // reference at(size_type __i) noexcept {}
-
-  // const_reference at(size_type __i) const noexcept {}
-
   value_type front() noexcept {
     return this->empty() ? static_cast<value_type>(0) : this->__data[__offset];
   }
 
-  // const_reference front() const noexcept {}
-
   value_type back() noexcept {
     return this->empty() ? static_cast<value_type>(0)
-                         : this->__data[__count + __offset - 1];
+                         : this->__data[(__count + __offset - 1) % __max_size];
   }
 
-  // const_reference back() const noexcept {}
-
-  // void push_front(value_type __v) {}
-
-  void push_back(value_type __v) {
-    if (__count + __offset == __max_size)
+  void push_front(value_type __v) {
+    if (__count == __max_size)
       this->resize(this->size() * 2);
 
-    this->__data[__count + __offset] = __v;
+    this->__data[__offset ? --__offset : __offset = __max_size - 1] = __v;
+    ++this->__count;
+  }
+
+  void push_back(value_type __v) {
+    if (__count == __max_size)
+      this->resize(this->size() * 2);
+
+    this->__data[(__count + __offset) % __max_size] = __v;
     ++this->__count;
   }
 
   void pop_front() {
     if (!this->empty()) {
-      ++this->__offset;
+      ++this->__offset %= __max_size;
       --this->__count;
     }
   }
